8d382ffa556bcc3ae39862df892c385d08fb1e8e
Vallabhbhagat <bhagatvallabh863@gmail.com>
2026-01-23 18:35:57 +0530

    added update and delete api

--- server/routes/clientRoute.js
+++ server/routes/clientRoute.js
@@ -5,11 +5,11 @@ const Client = require("../models/Client.js")
 router.post("/add", async (req, res) => {
     try {
         const { name, email } = req.body;
-        
+
         if (!name || !email) {
             return res.status(400).json({ message: "Name and email are required" });
         }
-        
+
         const clientExists = await Client.findOne({ email })
         if (clientExists) {
             return res.status(409).json({ message: "client already exist" });
@@ -19,14 +19,50 @@ router.post("/add", async (req, res) => {
             name,
             email
         });
-        
+
         await client.save();
 
         return res.status(201).json(client);
     } catch (error) {
-        return res.status(500).json({ error: error.message })
+        return res.status(500).json({ error: error.message });
     }
 
+});
+
+router.put("/:id", async (req, res) => {
+    try {
+        const client = await Client.findByIdAndUpdate(
+            req.params.id,
+            {
+                name: req.body.name,
+                email: req.body.email
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+
+        if (!client) return res.status(404).json({ message: 'Not found' });
+
+        return res.status(200).json(client);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const client = await Client.findByIdAndDelete(req.params.id);
+        
+        if (!client) {
+            return res.status(404).json({ message: "Client not found" });
+        }
+
+        return res.status(200).json({ message: "Client deleted successfully" });
+    } catch (error) {
+        res.json({message: error.message});
+    }
 });
 
 module.exports = router;
