e0c15136f7a8e1db2d22bd1fa4a6d35251ff9512
Vallabhbhagat <bhagatvallabh863@gmail.com>
2026-03-09 09:35:15 +0530

    Deploy

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://client-flow-nu.vercel.app/",
+    origin: "https://client-flow-1s3g.vercel.app/",
     credentials: true
 }));
 app.use(cookieparser());
